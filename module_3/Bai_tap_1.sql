cce7a70f0d9b7fb4a38441fc2042798f82d05cc8
nghia20480 <nghia20480@gmail.com>
2023-03-10 21:08:24 +0700

    nop bai

--- module_3/Bai_tap_1.sql
+++ module_3/Bai_tap_1.sql
@@ -1,1 +1,6 @@
-SELECT * FROM student_manager.teacher;
+SELECT * FROM student_manager.class;
+insert into student_manager.class(id,name)
+value(9,'bon');
+select*from student_manager.teacher;
+insert into student_manager.teacher(id,name,age,country)
+value(5,'ka',55,'bachan');
